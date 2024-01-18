fe76ecf5116d2b57b926888c7012cc1473ed1ee0
gab <gabrielarcega01@gmail.com>
2024-01-19 02:47:56 +0800

    tailwind view width fix and google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,16 +9,16 @@ module.exports = {
   theme: {
     extend: {
       height: {
-        "10v": "10vh",
-        "20v": "20vh",
-        "30v": "30vh",
-        "40v": "40vh",
-        "50v": "50vh",
-        "60v": "60vh",
-        "70v": "70vh",
-        "80v": "80vh",
-        "90v": "90vh",
-        "100v": "100vh",
+        '10v': '10vh',
+        '20v': '20vh',
+        '30v': '30vh',
+        '40v': '40vh',
+        '50v': '50vh',
+        '60v': '60vh',
+        '70v': '70vh',
+        '80v': '80vh',
+        '90v': '90vh',
+        '100v': '100vh',
 			},
       colors: {
         'violet-d': {
@@ -41,8 +41,20 @@ module.exports = {
         'blue-d': '#31507F', //dark blue text
       },
       fontFamily: {
-        'poppins': ['Poppins']
-      }
+        'pop': ['Poppins']
+      },
+      width: {
+        '10v': '10vw',
+        '20v': '20vw',
+        '30v': '30vw',
+        '40v': '40vw',
+        '50v': '50vw',
+        '60v': '60vw',
+        '70v': '70vw',
+        '80v': '80vw',
+        '90v': '90vw',
+        '100v': '100vw',
+			}
     },
   },
   plugins: [],

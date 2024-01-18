d74679679c0c9a74505e3c0159738eb123b13f2b
stanleypanag <gineiko9@gmail.com>
2024-01-19 00:08:40 +0800

    landing page finished

---
+++ src/before_auth/landingpage.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Styling -->
+    <link rel="stylesheet" href="../assets/style/landingpage.css">
+    <link rel="stylesheet" href="../global.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="left">
+           <div class="left-content">
+
+                <div class="image-container">
+                    <img src="../assets/img/logo.png">
+                </div>
+
+                <div class="text-container">
+                    <label>Track and Save <br> Electricity  <strong style="font-weight: boldest;">Smartly</strong></label>
+                    <a href="../before_auth/loginpage.php">
+                    <button >Let's get Started</button>
+                    </a>
+                </div>
+                
+                <div class="social-container">
+                    <img src="../assets/img/fb.png">
+                    <img src="../assets/img/twiter.png">
+                    <img style="width: 7vh;" src="../assets/img/coin.png">
+                </div>
+
+           </div>
+        </div>
+        <div class="right">
+            
+        </div>
+    </div>
+</body>
+
+</html>

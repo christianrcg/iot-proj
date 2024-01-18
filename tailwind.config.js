672c22e40270663957fe58d8e7eb014a540a87ad
gab <gabrielarcega01@gmail.com>
2024-01-18 18:23:37 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,php}",
+    "./src/**/*"
+    //"./src/after_auth/*.{html,js,php}",
+    //"./src/before_auth/*.{html,js,php}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

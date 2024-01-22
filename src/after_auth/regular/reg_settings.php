9ce71ce565839466af232e38b9a9aac3dbbbd686
Stanley Panag <130243031+stanleypanag@users.noreply.github.com>
2024-01-22 22:48:05 +0800

    Ajax Done

---
+++ src/after_auth/regular/reg_settings.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../regular/style/reg_notification.css">
+    <title>Regular Homepage</title>
+</head>
+
+<body>
+    <div class="settings">
+        <div class="settings-text">
+            <h1>Settings</h1>
+        </div>
+    </div>
+</body>
+
+</html>

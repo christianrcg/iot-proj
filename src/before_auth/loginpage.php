7adc35b6f3e517724331064b33e064f35ae9eeb5
gab <gabrielarcega01@gmail.com>
2024-01-23 01:59:36 +0800

    Register Login Pages, sql update
    paki import sayo sql kasi need yon, delete mo muna yung sa phpmyadmin mo then import mo yung nandito,
    
    admin account:
    username: admin
    username: admin
    
    regular:
    username: gab
    passsword: gab

---
+++ src/before_auth/loginpage.php
@@ -0,0 +1,105 @@
+<?php
+require_once('../functions/database/db_connect.php');
+session_start();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HEO | Login</title>
+    <link rel="stylesheet" href="../assets/style/register-login.css">
+    <!-- browser site icon -->
+    <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/browser-icon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/browser-icon/favicon-16x16.png">
+    <link rel="manifest" href="../assets/img/browser-icon/site.webmanifest">
+    <!-- additional css -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <main class="main-cont">
+        <section class="section-cont">
+            <img src="../assets/img/LOGO-removebg-preview.png">
+        </section>
+        <section class="section-cont">
+            <div class="section-content box-shadow">
+                <form id="loginUserForm">
+                    <div class="input-cont">
+                        <label for="username"> Username </label>
+                        <input type="text" name="username" required>
+                    </div>
+                    <div class="input-cont mb-zero">
+                        <label for="password"> Password </label>
+                        <input type="password" name="password" required>
+                    </div>
+                    <div class="input-cont forgot-password">
+                        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=gabrielarcega01@gmail.com" target="_blank">Forgot Password?</a>
+                    </div>
+                    <div class="input-cont center mb-s">
+                        <button class="btn-main box-shadow" type="submit">Login</button>
+                    </div>
+                    <div class="input-cont center mb-zero">
+                        <p>Don't have an account? <a href="registerpage.php" id="register_link">Register</a></p>
+                    </div>
+                </form>
+            </div>
+        </section>
+    </main>
+
+    <script src="../assets/jquery-3.7.1.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+
+    <script>
+        $(document).on('submit', '#loginUserForm', function(e) {
+            e.preventDefault();
+
+            let formData = new FormData(this);
+            formData.append("login_user", true);
+
+            $.ajax({
+                type: 'POST',
+                url: '../functions/auth/login_user.php',
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function(response) {
+                    let res = jQuery.parseJSON(response);
+                    alertify.set('notifier', 'position', 'top-center');
+
+                    if (res.status == 200) {
+                        let alertModal = alertify.alert()
+                            .setting({
+                                'label': 'OK',
+                                'message': res.message + ' Go to Homepage?',
+                                'onok': function() {
+                                    alertify.success(res.message + ' Redirecting to Homepage');
+
+                                    setTimeout(function() {
+                                        if (res.user_role == 'admin') {
+                                            window.location.href = '../components/admin_sidebar.php'
+                                        } else {
+                                            window.location.href = '../components/reg_sidebar.php'
+                                        }
+                                    }, 1500);
+                                }
+                            });
+                        alertModal.setHeader('<p><i class="fa-regular fa-circle-check" style="color: #009439;"></i> &nbsp Login Successful</p>');
+                        alertModal.show();
+
+                    } else if (res.status == 500 || 401 || 404) {
+                        let errorNotif = alertify.error(res.message);
+                        errorNotif.setHeader('<p><i class="fa-regular fa-circle-xmark" style="color: #c83c3c;"></i> &nbsp Error</p>');
+                        errorNotif.show();
+                    }
+                }
+            });
+        });
+    </script>
+</body>
+
+</html>
---
+++ src/functions/auth/register_user.php
@@ -0,0 +1,42 @@
+<?php
+include('../database/db_connect.php');
+
+if (isset($_POST['register_user'])) {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+
+    //Hash Password
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    //if the fields are not null, checks if the inputted username has a duplicate in existing table
+    $checkUsernameQuery = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username'");
+    $sameUsernameExists = mysqli_fetch_assoc($checkUsernameQuery);
+
+    if ($sameUsernameExists) {
+        $res = [
+            'status' => 409,
+            'message' => 'Username already taken'
+        ];
+        echo json_encode($res);
+        return;
+    }
+
+    //if the username is available -> proceeds to insert the username
+    $registerUserQuery = mysqli_query($conn, "INSERT INTO users (username, email, password) VALUES ('$username', '$email','$hashed_password')");
+    if ($registerUserQuery) {
+        $res = [
+            'status' => 200,
+            'message' => 'User Created'
+        ];
+        echo json_encode($res);
+        return;
+    } else {
+        $res = [
+            'status' => 500,
+            'message' => 'Internal Server Error'
+        ];
+        echo json_encode($res);
+        return;
+    }
+}
